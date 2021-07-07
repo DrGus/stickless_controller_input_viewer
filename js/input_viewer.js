5724c8735e62f652ff60cc6d60867a0a5e4882a0
Agustí Montes <a.gus493@gmail.com>
2021-07-08 00:18:30 +0200

    Added a Debug mode.
    Added a new debug mode to change the input maping, in case it doesn't work out of the box.
    
    Also, changed the name of some files.

--- js/input_viewer.js
+++ js/input_viewer.js
@@ -1,29 +1,6 @@
 let haveEvents = 'GamepadEvent' in window;
 let haveWebkitEvents = 'WebKitGamepadEvent' in window;
 let controllers = {};
-let map = {};
-
-/**
- * My personal mapping of the inputs. If yours isn't the same, activate debug,
- * watch in the console the ID of every input and change it on the list.
- */
-
- map[14] = "path873"; // <-- left
- map[13] = "path873-1"; // <-- down
- map[15] = "path873-1-0"; // <-- right
- map[12] = "path873-1-0-9-2-5-7"; // <-- up
- map[2] = "path873-1-0-9-2"; // <-- square
- map[3] = "path873-1-0-9-2-5"; // <-- triangle
- map[5] = "path873-1-0-9-2-1"; // <-- R1
- map[4] = "path873-1-0-9-2-1-9"; // <-- L1
- map[0] = "path873-1-0-9"; // <-- X
- map[1] = "path873-1-0-9-9"; // <-- circle
- map[7] = "path873-1-0-9-5"; // <-- R2
- map[6] = "path873-1-0-9-5-1"; // <-- L2
-
- /**
-  * End of mapping
-  */
 
 var rAF =
   window.mozRequestAnimationFrame ||
@@ -61,6 +38,9 @@ function updateStatus() {
     for (let i=0; i<controller.buttons.length; i++) {
       if (buttonPressed(controller.buttons[i])) {
         $("#" + map[i]).attr("r", 7);
+        if(debug) {
+          console.log(i);
+        }
       } else {
         $("#" + map[i]).attr("r", 0);
       }
